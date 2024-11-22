977d9869fcfef14abd7309ac3fdeadca388ae41a
Bogaert Tom <146355981+BgrtTom@users.noreply.github.com>
2024-11-22 14:53:59 +0100

    src(CodeSource): Modification des codes sources pour l'écriture des résultats dans un fichier

--- CodeSource/Code1/src/Assignment102.java
+++ CodeSource/Code1/src/Assignment102.java
@@ -1,4 +1,6 @@
 // Estimate the value of Pi using Monte-Carlo Method, using parallel program
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -6,6 +8,7 @@ class PiMonteCarlo {
 	AtomicInteger nAtomSuccess;
 	int nThrows;
 	double value;
+	int nProcessors = 1; /* Runtime.getRuntime().availableProcessors(); nb processeur */
 	class MonteCarlo implements Runnable {
 		@Override
 		public void run() {
@@ -21,7 +24,6 @@ class PiMonteCarlo {
 		this.value = 0;
 	}
 	public double getPi() {
-		int nProcessors = Runtime.getRuntime().availableProcessors(); /* nb processeur */
 		ExecutorService executor = Executors.newWorkStealingPool(nProcessors);
 		for (int i = 1; i <= nThrows; i++) {
 			Runnable worker = new MonteCarlo();
@@ -33,17 +35,39 @@ class PiMonteCarlo {
 		value = 4.0 * nAtomSuccess.get() / nThrows;
 		return value;
 	}
+
+	public AtomicInteger getnAtomSuccess() {
+		return nAtomSuccess;
+	}
+	public int getnProcessors() {
+		return nProcessors;
+	}
 }
 public class Assignment102 {
 	public static void main(String[] args) {
-		PiMonteCarlo PiVal = new PiMonteCarlo(100000);
+		int ntot = 1000000;
+		PiMonteCarlo PiVal = new PiMonteCarlo(ntot);
 		long startTime = System.currentTimeMillis();
 		double value = PiVal.getPi();
 		long stopTime = System.currentTimeMillis();
-		System.out.println("Approx value:" + value);
+		System.out.println("\nApprox value: " + value);
+		System.out.println("Error: " + (Math.abs((value - Math.PI)) / Math.PI));
+		System.out.println("Ntot: " + ntot);
+		System.out.println("Ncible: " + PiVal.getnAtomSuccess());
 		System.out.println("Difference to exact value of pi: " + (value - Math.PI));
-		System.out.println("Error: " + (value - Math.PI) / Math.PI * 100 + " %");
-		System.out.println("Available processors: " + Runtime.getRuntime().availableProcessors());
-		System.out.println("Time Duration: " + (stopTime - startTime) + "ms");
+		System.out.println("Available processors: " + PiVal.getnProcessors());
+		System.out.println("Time Duration (ms): " + (stopTime - startTime));
+
+		System.out.println( (value +";"+ Math.abs((value - Math.PI)) / Math.PI) +";"+ ntot +";"+ PiVal.getnProcessors() +";"+ (stopTime - startTime));
+
+		try {
+			FileWriter fw = new FileWriter("XP_assignement102.txt",true);
+			fw.write((value +";"+ Math.abs((value - Math.PI)) / Math.PI) +";"+ ntot +";"+ PiVal.getnAtomSuccess() +";"+ PiVal.getnProcessors() +";"+ (stopTime - startTime) + "\n");
+			fw.close();
+		} catch (IOException ioe) {
+			System.err.println("IOException: " + ioe.getMessage());
+		}
 	}
 }
+
+/*nb point / nb processus / nb cible / temps / erreur /pi*/
